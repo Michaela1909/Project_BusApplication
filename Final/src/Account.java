685d3779c29bd9c15d796292e0e182e2d774d3a9
Ella <90807615+Michaela1909@users.noreply.github.com>
2022-04-14 21:51:47 +0700

    Update Account.java

--- Final/src/Account.java
+++ Final/src/Account.java
@@ -6,8 +6,6 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ResourceBundle;
 
-import javax.sql.StatementEvent;
-import javax.swing.JOptionPane;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -23,6 +21,9 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
 
 public class Account implements Initializable {
+
+    @FXML
+    private Button btnBack;
 
     @FXML
     private Label lb_userid;
@@ -41,90 +42,77 @@ public class Account implements Initializable {
     
     @FXML
     private TextField tf_password;
-    
-    //int tabel;
-    //int myIndex;
-    //int UserID;
-    //String sql;
-    //StatementEvent st;
+
     PreparedStatement pst;
     Connection con = connection.BusConnecition();
 
 
-@Override
-public void initialize(URL location, ResourceBundle resources) {
-    // TODO Auto-generated method stub
-    try {
-        Statement st = con.createStatement();
-        String sql = "SELECT User.UserID,User.FullName, User.Email, User.Password FROM User, Transaksi WHERE Transaksi.UserID = User.UserID ORDER BY Transaksi.TransaksiID DESC LIMIT 1";
-        //SELECT User.FullName, User.Email, User.Password FROM User, Transaksi WHERE Transaksi.UserID = User.UserID ORDER BY Transaksi.TransaksiID DESC LIMIT 1;
-        ResultSet rs = st.executeQuery(sql);
-        while(rs.next()){
-            lb_userid.setText(rs.getString(1));
-            tf_nama.setText(rs.getString(2));
-            tf_email.setText(rs.getString(3));
-            tf_password.setText(rs.getString(4));
-            
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        try {
+            Statement st = con.createStatement();
+            String sql = "SELECT User.UserID,User.FullName, User.Email, User.Password FROM User, Transaksi WHERE Transaksi.UserID = User.UserID ORDER BY Transaksi.TransaksiID DESC LIMIT 1";
+            ResultSet rs = st.executeQuery(sql);
+            while(rs.next()){
+                lb_userid.setText("User ID: " + rs.getString(1));
+                tf_nama.setText(rs.getString(2));
+                tf_email.setText(rs.getString(3));
+                tf_password.setText(rs.getString(4));
+                
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            e.getCause();
         }
-    } catch (Exception e) {
-        //TODO: handle exception
+        
     }
-    
-}
 
 
 
-@FXML
-void btnEdit(ActionEvent event) {
+    @FXML
+    void btnEdit(ActionEvent event) {
+    try {
+        PreparedStatement pst = con.prepareStatement
+        ("UPDATE User Set FullName=?, Email=?, Password =?, ConfirmPassword=? WHERE UserID = (SELECT UserID FROM Transaksi ORDER BY TransaksiID DESC LIMIT 1)");
+            pst.setString(1, tf_nama.getText());
+            pst.setString(2, tf_email.getText());
+            pst.setString(3, tf_password.getText());
+            pst.setString(4, tf_password.getText());
 
-    /*myIndex = tabel.getSelectionsModel().getSelectedIndex();
-
-    UserID = Integer.parseInt(String.valueOf(table.getitem().get(myIndex).getUserID()));
-    nama = tf_nama.getText();
-    email = tf_email.getText();
-    password = tf_password.getText();*/
-   int result = 0;
-   try {
-    PreparedStatement pst = con.prepareStatement
-    ("UPDATE User Set FullName=?, Email=?, Password =?, ConfirmPassword=? WHERE UserID = (SELECT UserID FROM Transaksi ORDER BY TransaksiID DESC LIMIT 1)");
-        pst.setString(1, tf_nama.getText());
-        pst.setString(2, tf_email.getText());
-        pst.setString(3, tf_password.getText());
-        pst.setString(4, tf_password.getText());
-
-    if(pst.executeUpdate() !=0) {
-            Alert alert = new Alert(AlertType.INFORMATION);
-            alert.setTitle("Error");
-            alert.setHeaderText("Edit Gagal");
-            alert.setContentText("Check Di data base ");
-            alert.showAndWait();
-            result = 1;
-    }
-    else
-    {
-        Alert alert = new Alert(AlertType.INFORMATION);
+            if(pst.executeUpdate() ==-1) {
+                Alert alert = new Alert(AlertType.INFORMATION);
+                alert.setTitle("Error");
+                alert.setHeaderText("Edit Gagal");
+                alert.setContentText("Check Di data base ");
+                alert.showAndWait();    
+            }else{
+                Alert alert = new Alert(AlertType.INFORMATION);
                 alert.setTitle("Berhasil");
-                alert.setHeaderText("Anda berhasil mengirimkan pesan anda!");
+                alert.setHeaderText("Anda berhasil merubah profil akun!");
                 alert.setContentText("Terima kasih.");
                 alert.showAndWait();  
-                
-                tf_nama.setText(null);
-                tf_email.setText(null);
-                tf_password.setText(null);
-                lb_userid.setText(null);
-    }
-      
-        } catch (Exception e) {
-       //TODO: handle exception
-   }
-}
+                    
+            }
         
-@FXML
-void btnLogout(ActionEvent event) throws IOException {
-    Stage stage = (Stage) Logout.getScene().getWindow();
+        } catch (Exception e) {
+        e.printStackTrace();
+        e.getCause();
+    }
+    }
+            
+    @FXML
+    void btnLogout(ActionEvent event) throws IOException {
+        Stage stage = (Stage) Logout.getScene().getWindow();
         Parent root = FXMLLoader.load(getClass().getResource("Home.fxml"));
         stage.setTitle("Bus");
         stage.setScene(new Scene(root));
     }
 
+    @FXML
+    void back(ActionEvent event) throws IOException {
+        Stage stage = (Stage) btnBack.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("MainPage.fxml"));
+        stage.setTitle("Bus");
+        stage.setScene(new Scene(root));
+    }
 }
