b87ab76fdf304fa35cd3bb2b422e292e45af8795
Ella <90807615+Michaela1909@users.noreply.github.com>
2022-04-14 21:38:53 +0700

    Update AdminJadwalBus.java

--- Final/src/AdminJadwalBus.java
+++ Final/src/AdminJadwalBus.java
@@ -3,7 +3,9 @@ import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDate;
 import java.util.LinkedList;
 import java.util.ResourceBundle;
 
@@ -16,6 +18,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
@@ -36,6 +39,12 @@ public class AdminJadwalBus implements Initializable{
 
     @FXML
     private Button btn_tambahkan;
+
+    @FXML
+    private Button btn_Edit;
+
+    @FXML
+    private Button btn_Hapus;
 
     @FXML
     private TableView<jadwal> jadwalBus;
@@ -77,7 +86,7 @@ public class AdminJadwalBus implements Initializable{
     private TextField tf_harga;
 
     @FXML
-    private TextField tf_kapasitas;
+    private Label label_Kapasitas;
 
     @FXML
     private TextField tf_namaBus;
@@ -96,13 +105,104 @@ public class AdminJadwalBus implements Initializable{
         stage.setScene(new Scene(root));
     }
 
+
+    @FXML
+    void Edit(ActionEvent event) {
+        try {
+            pst = con.prepareStatement("UPDATE JadwalBus SET NamaBus = ?, Tanggal = ?, Harga = ?, Asal = ?, Tujuan = ?, Waktu = ? WHERE BusID = ?");
+            pst.setString(1, tf_namaBus.getText());
+            pst.setString(2, String.valueOf(tanggal.getValue()));
+            pst.setInt(3, Integer.parseInt(tf_harga.getText()));
+            pst.setString(4, tf_asal.getText());
+            pst.setString(5, tf_tujuan.getText());
+            pst.setString(6, tf_waktu.getText());
+            pst.setInt(7, Integer.parseInt(tf_BusID.getText()));
+            int status = pst.executeUpdate();
+            if(status==-1){
+                Alert alert = new Alert(AlertType.INFORMATION);
+                alert.setTitle("Error");
+                alert.setHeaderText("Terjadi kesalahan");
+                alert.setContentText("Silahkan coba lagi!");
+                alert.showAndWait();
+            }else{
+                Alert alert = new Alert(AlertType.INFORMATION);
+                alert.setTitle("Berhasil");
+                alert.setHeaderText("Selamat!");
+                alert.setContentText("Anda Berhasil Mengubah Jadwal Bus.");
+                alert.showAndWait();
+                clear();
+                getBusList();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            e.getCause();
+        }
+    }
+
+    @FXML
+    void Hapus(ActionEvent event) {
+        try {
+            pst = con.prepareStatement("DELETE FROM Seat WHERE BusID = ?");
+            pst.setInt(1, Integer.parseInt(tf_BusID.getText()));
+            pst.executeUpdate();
+            PreparedStatement pst3 = con.prepareStatement("DELETE FROM Transaksi WHERE BusID = ?");
+            pst3.setInt(1, Integer.parseInt(tf_BusID.getText()));
+            pst3.executeUpdate();
+            PreparedStatement pst2 = con.prepareStatement("DELETE FROM JadwalBus WHERE BusID = ?");
+            pst2.setInt(1, Integer.parseInt(tf_BusID.getText()));
+            int status = pst2.executeUpdate();
+            if(status == -1){
+                Alert alert = new Alert(AlertType.INFORMATION);
+                alert.setTitle("Error");
+                alert.setHeaderText("Terjadi kesalahan");
+                alert.setContentText("Silahkan coba lagi!");
+                alert.showAndWait();
+            }else{
+                Alert alert = new Alert(AlertType.INFORMATION);
+                alert.setTitle("Berhasil");
+                alert.setHeaderText("Selamat!");
+                alert.setContentText("Anda Berhasil Mengubah Jadwal Bus.");
+                alert.showAndWait();
+                clear();
+                getBusList(); 
+            }
+        } catch (SQLException e) {
+            Alert alert = new Alert(AlertType.INFORMATION);
+            alert.setTitle("Error");
+            alert.setHeaderText("Terjadi kesalahan");
+            alert.setContentText("Silahkan coba lagi!");
+            alert.showAndWait();
+            tf_BusID.setText(null);
+            tf_namaBus.setText(null);
+            label_Kapasitas.setText(Integer.toString(34));
+            tanggal.setValue(null);
+            tf_asal.setText(null);
+            tf_tujuan.setText(null);
+            tf_harga.setText(null);
+            tf_waktu.setText(null);
+        }
+    }
+
+    @FXML
+    void getSelected(MouseEvent event) {
+        int index = jadwalBus.getSelectionModel().getSelectedIndex();
+        tf_BusID.setText(Integer.toString(tc_BusID.getCellData(index)));
+        tf_namaBus.setText(tc_NamaBus.getCellData(index));
+        label_Kapasitas.setText(Integer.toString(tc_Kapasitas.getCellData(index)));
+        tanggal.setValue(LocalDate.parse(tc_Tanggal.getCellData(index)));
+        tf_harga.setText(Integer.toString(tc_Harga.getCellData(index)));
+        tf_asal.setText(tc_Asal.getCellData(index));
+        tf_tujuan.setText(tc_Tujuan.getCellData(index));
+        tf_waktu.setText(tc_Waktu.getCellData(index));
+    }
+
     @FXML
     void tambah(ActionEvent event) {
         try {
             pst = con.prepareStatement("INSERT INTO JadwalBus(BusID, NamaBus, Kapasitas, Tanggal, Harga, Asal, Tujuan, Waktu) VALUES (?,?,?,?,?,?,?,?)");
             pst.setInt(1, Integer.parseInt(tf_BusID.getText()));
             pst.setString(2, tf_namaBus.getText());
-            pst.setInt(3, Integer.parseInt(tf_kapasitas.getText()));
+            pst.setInt(3, Integer.parseInt(label_Kapasitas.getText()));
             pst.setString(4, String.valueOf(tanggal.getValue()));
             pst.setInt(5, Integer.parseInt(tf_harga.getText()));
             pst.setString(6, tf_asal.getText());
@@ -115,17 +215,7 @@ public class AdminJadwalBus implements Initializable{
                 alert.setHeaderText("Selamat!");
                 alert.setContentText("Anda Berhasil Menambahkan Jadwal Bus Baru.");
                 alert.showAndWait();
-                jadwalBus.getItems().clear();
-                list.clear();
-                tf_BusID.setText(null);
-                tf_namaBus.setText(null);
-                tf_kapasitas.setText(Integer.toString(34));
-                tanggal.setValue(null);
-                tf_asal.setText(null);
-                tf_tujuan.setText(null);
-                tf_harga.setText(null);
-                tf_waktu.setText(null);
-
+                clear();
                 try {
                     Statement st = con.createStatement();
                     String sql = "SELECT * FROM JadwalBus";
@@ -149,7 +239,6 @@ public class AdminJadwalBus implements Initializable{
                 alert.showAndWait();
             }
         } catch (Exception e) {
-            //TODO: handle exception
             e.printStackTrace();
             e.getCause();
             Alert alert = new Alert(AlertType.INFORMATION);
@@ -162,9 +251,8 @@ public class AdminJadwalBus implements Initializable{
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        tf_kapasitas.setText(Integer.toString(34));
+        label_Kapasitas.setText(Integer.toString(34));
 
-        // TODO Auto-generated method stub
         tc_BusID.setCellValueFactory(new PropertyValueFactory<>("BusID"));
         tc_NamaBus.setCellValueFactory(new PropertyValueFactory<>("NamaBus"));
         tc_Kapasitas.setCellValueFactory(new PropertyValueFactory<>("Kapasitas"));
@@ -174,6 +262,10 @@ public class AdminJadwalBus implements Initializable{
         tc_Tujuan.setCellValueFactory(new PropertyValueFactory<>("Tujuan"));
         tc_Waktu.setCellValueFactory(new PropertyValueFactory<>("Waktu"));
         
+        getBusList();
+    }
+
+    public void getBusList(){
         try {
             Statement st = con.createStatement();
             String sql = "SELECT * FROM JadwalBus";
@@ -185,10 +277,20 @@ public class AdminJadwalBus implements Initializable{
             jadwalBus.getItems().clear();
             jadwalBus.getItems().addAll(list);
         } catch (Exception e) {
-            //TODO: handle exception
             e.printStackTrace();
             e.getCause();
         }
     }
-
+    public void clear(){
+        jadwalBus.getItems().clear();
+        list.clear();
+        tf_BusID.setText(null);
+        tf_namaBus.setText(null);
+        label_Kapasitas.setText(Integer.toString(34));
+        tanggal.setValue(null);
+        tf_asal.setText(null);
+        tf_tujuan.setText(null);
+        tf_harga.setText(null);
+        tf_waktu.setText(null);
+    }
 }
